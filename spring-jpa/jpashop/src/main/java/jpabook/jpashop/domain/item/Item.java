96dae9ace27a9e4a6893a4007e5f089ff122495a
joo1yeon <joo1yeon@naver.com>
2025-04-04 18:45:18 +0900

    📖 spring-jpa 6. 주문 도메인 개발_1

--- spring-jpa/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ spring-jpa/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -25,4 +25,17 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    //== 비즈니스 로직 ==//
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    public void removeStock(int quantity) {
+        int restStock = stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        stockQuantity = restStock;
+    }
+
 }
